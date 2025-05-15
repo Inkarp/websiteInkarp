617cfaea3ee6afd2aae4635d5616c061546b2392
Sharath Gounikadi <sharath@inkarp.co.in>
2025-05-15 14:22:59 +0530

    added vercel.json

--- fronted-inkarp/src/App.jsx
+++ fronted-inkarp/src/App.jsx
@@ -1,35 +1,31 @@
 // App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
+import MainLayout from "./components/MainLayout";
+
+// Pages
 import Home from "./components/pages/Home";
+import OurStory from "./components/pages/OurStory";
 import Verticals from "./components/pages/Verticals";
 import ContactUs from "./components/pages/ContactUs";
-import OurStory from "./components/pages/OurStory";
-import NotFound from "./components/pages/NotFound";
-// import Insights from "./components/pages/Insights";
-import Careers from "./components/pages/Careers";
-import MainLayout from "./components/MainLayout";
 import NewCareers from "./components/pages/Careers/NewCareers";
+import Careers from "./components/pages/Careers";
 import HeroSection from "./components/pages/Home/HeroSection";
-import ScrollToTop from "./ScrollToTop";
 import Catalyst from "./components/pages/Catalyst";
-import Blogs from "./components/pages/Home/Blogs";
-import NewsEvents from "./components/pages/Home/NewsEvents";
-import EventsPage from "./components/pages/NewsAndEvents/EventsPage";
 import BlogsPage from "./components/pages/NewsAndEvents/BlogsPage";
+import EventsPage from "./components/pages/NewsAndEvents/EventsPage";
 import ProductProfile from "./components/pages/ProductProfile";
-// import Blogs from "./components/pages/insights/Blogs";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
   return (
     <Router>
-      <></>
       <ScrollToTop />
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="/about" element={<OurStory />} />
           <Route path="/verticals" element={<Verticals />} />
-          {/* <Route path="/insights&updates/blogs" element={<Blogs />} /> */}
           <Route path="/careers" element={<NewCareers />} />
           <Route path="/careers/positions" element={<Careers />} />
           <Route path="/contact" element={<ContactUs />} />
